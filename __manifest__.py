5afe17742ea944b45a14418b8a31d73a3155c10f
Abin Joshua Hermon <abinjoshua3@gmail.com>
2026-04-25 14:51:33 +0530

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Purchase Delivery Rating",
+    'version': "1.0",
+    'licence': "LGPL-3",
+    'author': "Cybrosys",
+    'website': "http://www.cybrosys.com",
+    'sequence': 1,
+    'application': True,
+    'depends': ['purchase'],
+    'data': ['views/purchase_order_views.xml'],
+    'auto_install': True,
+}
